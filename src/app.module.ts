6acb0268602894f2a92970658272b5dae0ea098c
Matthias Mulugeta <matthias22m@gmail.com>
2024-12-19 11:25:54 +0300

    add sqlite3 support and configure TypeORM in app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,17 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './users/users.entity';
 
 @Module({
-  imports: [UsersModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'data.db',
+      entities: [User],
+      synchronize: true,
+    }),
+    UsersModule,
+  ],
 })
 export class AppModule {}
